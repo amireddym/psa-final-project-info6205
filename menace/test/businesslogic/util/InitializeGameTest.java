8d11ab0be145a9c0e156a7295e474a5784803865
Anshita Verma <anshitaverma@Anshitas-MacBook-Pro.local>
2022-04-25 19:01:04 -0400

    Added additional test cases

--- menace/test/businesslogic/util/InitializeGameTest.java
+++ menace/test/businesslogic/util/InitializeGameTest.java
@@ -4,10 +4,52 @@
  */
 package businesslogic.util;
 
+import businesslogic.model.Bead;
+import businesslogic.model.Beads;
+import businesslogic.model.MatchBox;
+import businesslogic.model.MenaceGame;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
 /**
  *
  * @author manojreddy
  */
 public class InitializeGameTest {
     
+    @Test
+    public void initializeTest(){
+        MenaceGame menaceGame= InitilaizeGame.initialize();
+        assertNotNull(menaceGame);
+        assertNotNull(menaceGame.getMenaceTrainedState());
+        assertNotNull(menaceGame.getMenaceTrainedState().getMatchBoxes());
+        Assert.assertEquals(menaceGame.getMenaceTrainedState().getMatchBoxes().size(), 631);
+    }
+    
+    @Test
+    public void addaddInitalState() {
+        
+        Map<MatchBox, Beads> matchBoxes = new HashMap<>();
+        InitilaizeGame.addInitalState(matchBoxes);
+        MatchBox matchBox = matchBoxes.keySet().stream().findFirst().get();
+        Assert.assertEquals(matchBox.getState().length,9);
+        for(int i=0;i<9;i++) {
+            Assert.assertTrue(matchBox.getState()[i]==0);
+        }
+        
+        Beads beads = matchBoxes.get(matchBox);
+        Assert.assertEquals(beads.getPositions().size(), 3);
+        List<Bead> beadList = beads.getPositions();
+        Assert.assertEquals(beadList.get(0).getBoardPosition(), 0);
+        Assert.assertEquals(beadList.get(1).getBoardPosition(), 1);
+        Assert.assertEquals(beadList.get(2).getBoardPosition(), 4);
+        
+        Assert.assertEquals(beadList.get(0).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beadList.get(1).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beadList.get(2).getCurrentCount(), MenaceConstants.ALPHA);
+        
+    }
 }
--- menace/test/businesslogic/util/StateInitializerTest.java
+++ menace/test/businesslogic/util/StateInitializerTest.java
@@ -4,10 +4,46 @@
  */
 package businesslogic.util;
 
+import businesslogic.model.Beads;
+import businesslogic.model.MatchBox;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  *
  * @author manojreddy
  */
 public class StateInitializerTest {
+    
+    @Test
+    public void generateDefaultBeadsFromMatchboxStateTest() {
+        
+        MatchBox matchBox = new MatchBox(new int[9]);
+        Assert.assertEquals(StateInitializer.generateDefaultBeadsFromMatchboxState(matchBox).getPositions().size(),9);
+        Beads beads  = StateInitializer.generateDefaultBeadsFromMatchboxState(matchBox);
+        Assert.assertEquals(beads.getPositions().get(0).getBoardPosition(), 0);
+        Assert.assertEquals(beads.getPositions().get(1).getBoardPosition(), 1);
+        Assert.assertEquals(beads.getPositions().get(2).getBoardPosition(), 2);
+        Assert.assertEquals(beads.getPositions().get(3).getBoardPosition(), 3);
+        Assert.assertEquals(beads.getPositions().get(4).getBoardPosition(), 4);
+        Assert.assertEquals(beads.getPositions().get(5).getBoardPosition(), 5);
+        Assert.assertEquals(beads.getPositions().get(6).getBoardPosition(), 6);
+        Assert.assertEquals(beads.getPositions().get(7).getBoardPosition(), 7);
+        Assert.assertEquals(beads.getPositions().get(8).getBoardPosition(), 8);
+        
+        Assert.assertEquals(beads.getPositions().get(0).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(1).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(2).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(3).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(4).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(5).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(6).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(7).getCurrentCount(), MenaceConstants.ALPHA);
+        Assert.assertEquals(beads.getPositions().get(8).getCurrentCount(), MenaceConstants.ALPHA);
+        
+        
+    }
+    
+    
     
 }
